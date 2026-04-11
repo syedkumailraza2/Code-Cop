f0906bb2e8a168d3e48d47fa81f2b93d60993c89
Syed Kumail Raza <syedkumailraza28@gmail.com>
2026-04-11 18:25:32 +0530

    deployed the backend and changed the baseurl

--- client/next.config.ts
+++ client/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/evaluate",
-        destination: "http://localhost:8000/evaluate",
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/evaluate`,
       },
     ];
   },
